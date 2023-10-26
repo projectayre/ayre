90329727f393ee66bc8682b34031121aecc225a7
vasu7400 <vasu7400@me.com>
2023-10-27 01:57:33 +0530

    Tech Stack update
    - Updated to Typescript
    - Installed NextUI 2

---
+++ frontend/app/page.tsx
@@ -0,0 +1,22 @@
+import NextLink from "next/link";
+import { Link } from "@nextui-org/link";
+import { Snippet } from "@nextui-org/snippet";
+import { Code } from "@nextui-org/code"
+import { button as buttonStyles } from "@nextui-org/theme";
+import { siteConfig } from "@/config/site";
+import { title, subtitle } from "@/components/primitives";
+import { GithubIcon } from "@/components/icons";
+
+export default function Home() {
+	return (
+		<section className="flex flex-row items-center justify-evenly gap-4 py-8 md:py-10 border-2">
+			<div className="flex flex-col items-center justify-center gap-4 border-2">
+
+			</div>
+			
+			<div className="flex flex-col items-center justify-center gap-4 border-2">
+			
+			</div>			
+		</section>
+	);
+}
