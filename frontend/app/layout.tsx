a6e1e3e654a4fec832a18b6154312eaaff5821fd
vasu7400 <vasu7400@me.com>
2023-11-03 14:58:02 +0530

    Quackity

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -40,7 +40,7 @@ export default function RootLayout({
 				<Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
 					<div className="relative flex flex-col h-screen">
 						<Navbar />
-						<main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+						<main className="container mx-auto max-w-7xl pt-1 px-6 flex-grow">
 							{children}
 						</main>
 					</div>
--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -7,34 +7,47 @@ import { button as buttonStyles } from "@nextui-org/theme";
 import { siteConfig } from "@/config/site";
 import { title, subtitle } from "@/components/primitives";
 import { GithubIcon } from "@/components/icons";
-import { Divider, Textarea } from "@nextui-org/react";
-import { SketchfabModel }  from '../components/SketchfabModel';
-
-// import { Model } from '../components/New_surface_study_2';
-
+import { Canvas } from '@react-three/fiber';
+import { Divider, Textarea, Text, Image, Card } from "@nextui-org/react";
+import { SketchfabModel } from '../components/SketchfabModel';
 
 export default function Home() {
 	return (
-		<section className="flex flex-row items-center justify-evenly h-4/5 gap-4 md:py-2 border-2">
-			<div className="flex flex-col items-center justify-center w-1/2 h-full border-2 max-w-full">
-				<h1 className="text-3xl font-bold"> Insert 3D model here </h1>
-				<SketchfabModel />
-				{/* <div class="sketchfab-embed-wrapper"> <iframe title="New Surface study 2" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/4e821134e2994b8d8164dd7255fe9fd6/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/new-surface-study-2-4e821134e2994b8d8164dd7255fe9fd6?utm_medium=embed&utm_campaign=share-popup&utm_content=4e821134e2994b8d8164dd7255fe9fd6" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> New Surface study 2 </a> by <a href="https://sketchfab.com/tamminen?utm_medium=embed&utm_campaign=share-popup&utm_content=4e821134e2994b8d8164dd7255fe9fd6" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> tamminen </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=4e821134e2994b8d8164dd7255fe9fd6" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div> */}
+		<section className="flex flex-col items-center h-3/4 border-2">
+			<div className="flex flex-row items-center justify-center w-full h-3/4 border-2 max-w-full">
+				<div className="flex flex-col items-center justify-center w-1/2 h-full border-2">
+					<SketchfabModel />
+				</div>
 
-				<Divider />
+				<div className="flex flex-col items-center justify-start w-1/2 h-full border-2">
+					<div className="flex flex-col items-center justify-center w-full h-1/5 border-2">
+						<Card css={{ mw: "95%" }}>
+							<Card.Body>
+								<Text>A basic card</Text>
+							</Card.Body>
+						</Card>
+					</div>
+					<div className="flex flex-col items-center justify-center w-full h-1/2 border-2 flex-grow">
+
+						<Image
+							isBlurred
+							width={400}
+							src="https://nextui-docs-v2.vercel.app/images/album-cover.png"
+							alt="NextUI Album Cover"
+							classNames="m-10"
+						/>
+					</div>
+				</div>
+			</div>
+
+			<div className="flex flex-row items-center justify-center w-full h-full border-2 px-6">
 				<Textarea
+					fullWidth={true}
 					label="Description"
 					labelPlacement="outside"
 					placeholder="Enter your description"
-					size="full"
+					className=""
 				/>
-			</div>
-
-			<div className="flex flex-col items-center justify-center gap-4 w-1/2 h-full border-2">
-				<h1 className="text-3xl font-bold"> Sentiment Plot </h1>
-				<h1 className="text-3xl font-bold"> Image Plot </h1>
-				<Divider />
-				<h1 className="text-3xl font-bold"> Answer </h1>
 			</div>
 		</section>
 	);
