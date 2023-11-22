5238f7e0cb857142db50e00db387e6b06a93e842
Amee Madhani <e21cseu0171@bennett.edu.in>
2023-11-22 16:34:20 +0530

    change layout add chart

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,41 +1,37 @@
-'use client'
-import NextLink from "next/link";
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code"
-import { button as buttonStyles } from "@nextui-org/theme";
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
+"use client";
 import { Divider, Textarea } from "@nextui-org/react";
-import { SketchfabModel }  from '../components/SketchfabModel';
+import { SketchfabModel } from "../components/SketchfabModel";
+import { AnswerCard } from "@/components/answer-card";
+import { RadarChart } from "@/components/chart";
 
 // import { Model } from '../components/New_surface_study_2';
 
-
 export default function Home() {
-	return (
-		<section className="flex flex-row items-center justify-evenly h-4/5 gap-4 md:py-2 border-2">
-			<div className="flex flex-col items-center justify-center w-1/2 h-full border-2 max-w-full">
-				<h1 className="text-3xl font-bold"> Insert 3D model here </h1>
-				<SketchfabModel />
-				{/* <div class="sketchfab-embed-wrapper"> <iframe title="New Surface study 2" frameborder="0" allowfullscreen mozallowfullscreen="true" webkitallowfullscreen="true" allow="autoplay; fullscreen; xr-spatial-tracking" xr-spatial-tracking execution-while-out-of-viewport execution-while-not-rendered web-share src="https://sketchfab.com/models/4e821134e2994b8d8164dd7255fe9fd6/embed"> </iframe> <p style="font-size: 13px; font-weight: normal; margin: 5px; color: #4A4A4A;"> <a href="https://sketchfab.com/3d-models/new-surface-study-2-4e821134e2994b8d8164dd7255fe9fd6?utm_medium=embed&utm_campaign=share-popup&utm_content=4e821134e2994b8d8164dd7255fe9fd6" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> New Surface study 2 </a> by <a href="https://sketchfab.com/tamminen?utm_medium=embed&utm_campaign=share-popup&utm_content=4e821134e2994b8d8164dd7255fe9fd6" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;"> tamminen </a> on <a href="https://sketchfab.com?utm_medium=embed&utm_campaign=share-popup&utm_content=4e821134e2994b8d8164dd7255fe9fd6" target="_blank" rel="nofollow" style="font-weight: bold; color: #1CAAD9;">Sketchfab</a></p></div> */}
-
-				<Divider />
-				<Textarea
-					label="Description"
-					labelPlacement="outside"
-					placeholder="Enter your description"
-					size="full"
-				/>
-			</div>
-
-			<div className="flex flex-col items-center justify-center gap-4 w-1/2 h-full border-2">
-				<h1 className="text-3xl font-bold"> Sentiment Plot </h1>
-				<h1 className="text-3xl font-bold"> Image Plot </h1>
-				<Divider />
-				<h1 className="text-3xl font-bold"> Answer </h1>
-			</div>
-		</section>
-	);
+  return (
+    <section className="grid gap-4 ">
+      <div className=" grid grid-row-3 grid-flow-col items-center gap-2 p-8">
+        <div>
+          <AnswerCard />
+        </div>
+        <div className=" items-center justify-center max-w-full">
+          <SketchfabModel />
+        </div>
+        <div className="  justify-center h-full mt-20 ">
+          {/* <AnswerCard /> */}
+		  <RadarChart />
+          {/* <AnswerCard /> */}
+          {/* <Divider /> */}
+        </div>
+      </div>
+      <div>
+        <Divider />
+        <Textarea className="p-1 font-size-md"  
+          label="Description"
+          labelPlacement="outside"
+          placeholder="Enter your description"
+          size="full"
+        />
+      </div>
+    </section>
+  );
 }
---
+++ frontend/components/answer-card.tsx
@@ -0,0 +1,22 @@
+"use client";
+
+import React from "react";
+import {Card, CardHeader, CardBody, CardFooter, Divider, Link, Image} from "@nextui-org/react";
+
+export const AnswerCard = () => {
+	return (
+    <Card className="max-w-[400px]">
+      <CardHeader className="flex gap-3">
+        <div className="flex flex-col">
+          <p className="text-md">ANSWER</p>
+          <p className="text-small text-default-500">description of the answer</p>
+        </div>
+      </CardHeader>
+      <Divider/>
+      <CardBody>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      </CardBody>
+    </Card>
+		
+	);
+};
