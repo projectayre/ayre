27d6651a63a4126d17fd88883bc774f28c0c04a9
Amee Madhani <e21cseu0171@bennett.edu.in>
2023-11-27 02:03:34 +0530

    add input and output

--- frontend/components/answer-card.tsx
+++ frontend/components/answer-card.tsx
@@ -16,7 +16,7 @@ export const AnswerCard = () => {
       <CardBody>
 
       {/* can pass the image from the backend here */}
-      
+
       <Image
           src="test.jpg"
           alt="Image of the answer"
@@ -25,7 +25,7 @@ export const AnswerCard = () => {
           className="rounded-md"
         />
 
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit.</p>
       </CardBody>
     </Card>
 		
